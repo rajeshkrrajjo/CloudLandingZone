732b6122092326530f461d20f5c9af8c7f871dfb
Rajesh Kumar <rajeshkrrajjo@gmail.com>
2025-01-26 17:38:20 +0530

    fix: updateplan and apply

--- azure/providers.tf
+++ azure/providers.tf
@@ -1,4 +1,22 @@
 provider "azurerm" {
   features {}
-  use_oidc = true
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.7"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    key              = "terraform.tfstate"
+    use_oidc         = true
+    use_azuread_auth = true
+  }
+
 }
