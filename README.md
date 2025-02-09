06d33398c4276751e0740c361c9061eb9bc296ac
Rajesh Kumar <rajeshkrrajjo@gmail.com>
2025-02-09 19:43:57 +0530

    fix: udpdated backend

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CloudLandingZone
 Landing Zone code for multi Cloud
+
+# References
+https://github.com/abhishek6969/github-actions/blob/main/.github/workflows/terraform-apply.yml
+https://www.youtube.com/watch?v=cZt1xAV8L7E
+
+https://medium.com/bluetuple-ai/terraform-remote-state-on-azure-e939533493be
+
+https://learn.microsoft.com/en-us/azure/developer/terraform/authenticate-to-azure?tabs=bash
--- azure/deploy/providers.tf
+++ azure/deploy/providers.tf
@@ -1,13 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "backendrg"
+    storage_account_name = "strgbackend01"
+    container_name       = "terraformbackend"
+    key                  = "terraform.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.7"
-    }
-  }
-
 }
