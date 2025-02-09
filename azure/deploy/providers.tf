48ced34f88f8549beb840e731f6eb26558c478e6
Rajesh Kumar <rajeshkrrajjo@gmail.com>
2025-02-09 19:47:27 +0530

    fix: udpdated backend

--- azure/deploy/providers.tf
+++ azure/deploy/providers.tf
@@ -1,11 +1,4 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "backendrg"
-    storage_account_name = "strgbackend01"
-    container_name       = "terraformbackend"
-    key                  = "terraform.tfstate"
-  }
-}
+
 provider "azurerm" {
   features {}
 }
